c9a57f594036aa060c3d94392b994050d8428e4f
GAUCHOUX Xavier <xavier.gauchoux@soprasteria.com>
2023-11-16 13:25:50 +0100

    Docker branch

--- storeservice/src/main/java/com/fxmxracingteam/storeservice/jms/JmsConfig.java
+++ storeservice/src/main/java/com/fxmxracingteam/storeservice/jms/JmsConfig.java
@@ -1,21 +1,17 @@
-package com.fxmxracingteam.CommonConfigLib.config;
+package com.fxmxracingteam.storeservice.jms;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 public class JmsConfig {
-	
-	@Value("${spring.activemq.broker-url}")
-    private String tcp;
 
     @Bean
     public ActiveMQConnectionFactory connectionFactory(){
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL(tcp); // Remplacez hostname et port par vos valeurs
+        connectionFactory.setBrokerURL("tcp://activemq:61616"); // Remplacez hostname et port par vos valeurs
         connectionFactory.setUserName("admin"); // Optionnel
         connectionFactory.setPassword("admin"); // Optionnel
         connectionFactory.setTrustAllPackages(true);
